78c375a241ec2cc60a9f0779102be09ec28e6ef8
Shay <sleacy@googlemail.com>
2025-04-21 19:22:42 +0100

    Set up category view

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.views.generic import ListView
+from .models import Category
+
 
 # Create your views here.
+class Categories(generic.ListView):
+    queryset = Category.objects.all()
+    template_name = "chat/categories.html"
--- crafts/urls.py
+++ crafts/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("", include("chat.urls"), name="chat-urls"),
     path("summernote/", include("django_summernote.urls")),
     path("admin/", admin.site.urls),
 ]
