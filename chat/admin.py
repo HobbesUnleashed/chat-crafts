1b44eb2fbe93a19dcf95bb211353df36a85c60b7
Shay <sleacy@googlemail.com>
2025-04-21 19:14:12 +0100

    Create category model

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category
+from django_summernote.admin import SummernoteModelAdmin
+
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(SummernoteModelAdmin):
+    summernote_fields = (
+        "title",
+        "description",
+    )
--- crafts/settings.py
+++ crafts/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     ".herokuapp.com",
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django_summernote',
     "chat",
 ]
 
@@ -93,6 +94,7 @@ WSGI_APPLICATION = "crafts.wsgi.application"
 
 DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
+CSRF_TRUSTED_ORIGINS = ["https://*.herokuapp.com", "https://*.codeinstitute-ide.net/"]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
