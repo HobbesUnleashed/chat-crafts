4b044cb6f4bee794ba40799f580d73490c0d732d
Shay <sleacy@googlemail.com>
2025-04-21 21:26:40 +0100

    Readied for deployment

--- chat/models.py
+++ chat/models.py
@@ -15,3 +15,51 @@ class Category(models.Model):
 
     def __str__(self):
         return f"{ self.title } | { self.description }"
+
+
+class Post(models.Model):
+    AGE = (
+        (0, "5+"),
+        (1, "8+"),
+        (2, "10+"),
+        (3, "13+"),
+        (4, "15+"),
+        (5, "18+"),
+    )
+
+    SKILL = (
+        (0, 1),
+        (1, 2),
+        (2, 3),
+        (3, 4),
+        (4, 5),
+    )
+
+    title = models.CharField(max_length=200, unique=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="cat_id"
+    )
+    content = models.TextField()
+    materials = models.TextField()
+    time_taken = models.IntegerField(help_text="Time taken in minutes")
+    age = models.IntegerField(choices=AGE, default=0)
+    image = models.ImageField(
+        upload_to="craft-images/", default="craft-images/default_card.jpg"
+    )
+    media_url = models.URLField(
+        max_length=200,
+        blank=True,
+        null=True,
+        help_text="Link to your Youtube/Facebook, etc",
+    )
+    skill = models.IntegerField(choices=SKILL, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="craft_posts"
+    )
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
--- chat/urls.py
+++ chat/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.Categories.as_view(), name="home"),
+    path("posts/<int:category_id>/", views.PostList.as_view(), name="posts_list"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
