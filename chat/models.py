ebfa51d9c75a594d3ca2dabeac7113fac211510d
Shay <sleacy@googlemail.com>
2025-04-21 18:55:11 +0100

    Create category model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=30, unique=True)
+    description = models.TextField()
+    image = models.ImageField(
+        upload_to="category-images/", default="category-images/no-image.jpg"
+    )
+
+    class Meta:
+        ordering = ["id"]
+
+    def __str__(self):
+        return f"{ self.title } | { self.description }"
